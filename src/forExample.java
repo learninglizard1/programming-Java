5db963ca20e2fa9ed190f345df8dfd1b5f35b5c0
learninglizard1 <SachinSinghBade@myop.ac.nz>
2026-03-14 10:08:40 +1300

    Update forExample.java

--- src/forExample.java
+++ src/forExample.java
@@ -15,3 +15,38 @@ public class forExample {
 			System.out.println("Animal " + j + " name is " + animals[j]);		}
 	}
 }
+============================================================================================
+
+	import java.util.Scanner;
+
+public class ForExample1 {
+ 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Enter your first number: ");
+		int num = scanner.nextInt();		
+		
+		System.out.println("Enter your second number: ");
+		int num2 = scanner.nextInt();
+		
+		for(int i = num ; i <= num2; i++) {
+			System.out.println(i);
+		}
+ 
+	}
+	
+}
+
+============================================================================================
+public class ForExample3 {
+ 
+	public static void main(String[] args) {
+		String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+		
+		for(int i = days.length - 1; i >= 0; i--) {
+			System.out.println(days[i]);
+		}
+	}
+	
+}
